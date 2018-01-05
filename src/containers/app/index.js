b30448f1085c49cb1f74dc1d54f62712574f6a3e
Nardus van der Vyver <nvandervyver@retrorabbit.co.za>
2018-01-05 12:18:17 +0200

    Overall structure template

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Login from '../login'
 import Register from '../register'
+import Body from '../body'
+import Header from '../header'
+import Sidebar from '../sidebar'
+import Chat from '../chat'
 
 const App = () => (
     <div>
         <main>
+            <Route exact path="/" component={Login} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
+            <Route exact path="/body" component={Body} />
+            <Route exact path="/header" component={Header} />
+            <Route exact path="/sidebar" component={Sidebar} />
+            <Route exact path="/chat" component={Chat} />
         </main>
     </div>
 )
---
+++ src/containers/sidebar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom'
+import Login from '../login'
+import Register from '../register'
+import Body from '../body'
+
+const Sidebar = () => (
+    <div>
+        <p>Sidebar</p>
+    </div>
+)
+
+export default Sidebar;
