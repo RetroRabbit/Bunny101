ee2f38745f00fc72f3abecbd5cf5122e5d122b69
retrotshepo <tmabine@retrorabbit.co.za>
2018-01-08 12:46:20 +0200

    header: implemented changes; indentation and naming

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -3,34 +3,25 @@ import { Route } from 'react-router-dom'
 import Login from '../login'
 import Register from '../register'
 import Body from '../body'
-
-import _profileimg1 from './ic_account_circle_black_48dp.png'
-import _profileimg2 from './ic_schedule_black_48dp.png'
-
-
+import imageProfile from './ic_account_circle_black_48dp.png'
+import imageAbout from './ic_schedule_black_48dp.png'
 import './index.css'
 
 const Header = () => (
     <div id="header">
-	
-		<button class="rectangle-1">
-			NEW CHAT
-		</button>
+		<button id="buttonChat">NEW CHAT</button>
 		
-		<button class="rectangle-2">
-			NEW GROUP
-		</button>
+		<button id="buttonGroup">NEW GROUP</button>
 		
-		<div id="account-settings">
+		<div id="accountSettings">
+			<p id="chatName">Eddie Logan</p>
 			
-			<p class="chatname">Eddie Logan</p>
-
-			<button id="btn-square1">
-				<img src={_profileimg1} height="100%" width="100%"></img>
+			<button id="buttonProfile">
+				<img src={imageProfile} height="100%" width="100%"></img>
 			</button>
 			
-			<button id="btn-square1">
-				<img src={_profileimg2} alt={"logo"} height="100%" width="100%"></img>
+			<button id="buttonAbout">
+				<img src={imageAbout} alt={"logo"} height="100%" width="100%"></img>
 			</button>
 		</div>
 		
