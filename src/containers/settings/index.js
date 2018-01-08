e5bd4f1ef489a599531893f4aea1ec36bb027bc8
Seale Rapolai <1098005@students.wits.ac.za>
2018-01-08 12:07:24 +0200

    Finished settings interface design.

--- src/containers/settings/index.js
+++ src/containers/settings/index.js
@@ -2,27 +2,35 @@ import "./index.css"
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Profile_Pic from './user.png'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import pencil from './pencil-icon.png'
+import FontAwesome from 'react-fontawesome'
 
 const Settings = (props) =>(
     <div class="settings">
         <div class="settings-wrapper">
             <div class="settings-content">
-                <div class="profile-pic-container">
-                    <div class="profile-pic">
-                        <img src={Profile_Pic}></img>
+                <MuiThemeProvider>
+                    <div class="profile-pic-container">
+                        <div class="profile-pic">
+                            <img src={Profile_Pic}></img>
+                        </div>
                     </div>
-                </div>
-                <div class="user-details">
-                    <div class="user-name">
-                        <h1>James Jones</h1>
+
+                        <div class="user-details">
+                            <div class="user-name">
+                                <h1>James Jones <FontAwesome name='pencil' /></h1>
+
+                            </div>
+                            <div class="user-email">
+                                <p>james.jones@gmail.com <FontAwesome name='pencil' /></p>
+                            </div>
+                        </div>
+
+                    <div class="controls">
+                        <button>DONE</button>
                     </div>
-                    <div class="user-email">
-                        <p>james.jones@gmail.com</p>
-                    </div>
-                </div>
-                <div class="controls">
-                    <button>DONE</button>
-                </div>
+                </MuiThemeProvider>
             </div>
         </div>
     </div>
