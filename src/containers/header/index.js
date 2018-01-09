38da5c3d4a21cc7f2de2ca3402e572c39293d9f2
retrotshepo <tmabine@retrorabbit.co.za>
2018-01-09 14:41:08 +0200

    header: fixed indentation

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -4,27 +4,24 @@ import Login from '../login'
 import Register from '../register'
 import Body from '../body'
 import imageProfile from './ic_account_circle_black_48dp.png'
-import imageAbout from './ic_schedule_black_48dp.png'
+import imageAbout from './ic_fiber_manual_record_black_48dp.png'
 
-import FontAwesome from 'react-fontawesome'
 import FaSearch from 'react-icons/lib/fa/search';
 
 import './index.css'
 
 class HelloMessage extends React.Component {
 
-	
   render() {
     return (
 	<div id="header">
-
 		<div className="dropDown">
 			<button className="buttonRectangular" id="chatButton">NEW CHAT</button>
 			<div className="dropDownContent">
 				<form action="" class="chatSearchForm">
 					<input type="text" placeholder="Search..." name="search"/>
-				<button type="submit"><FaSearch /></button>
-			</form>
+					<button type="submit"><FaSearch /></button>
+				</form>
 			</div>
 		</div>
 		
@@ -33,17 +30,13 @@ class HelloMessage extends React.Component {
 			<div className="dropDownContent">
 				<form action="" class="groupSearchForm">
 					<input type="text" placeholder="Search..." name="search"/>
-				<button type="submit"><FaSearch /></button>
-				
-			</form>
+					<button type="submit"><FaSearch /></button>
+				</form>
 			</div>
 		</div>
 		
-		
 		<div id="accountSettings">
 			<p id="chatName">Eddie Logan</p>
-			
-			
 			<div className="dropDown">
 				<button className="buttonCircular" id="preferences">
 					<img src={imageProfile} height="100%" width="100%"></img>
@@ -53,18 +46,14 @@ class HelloMessage extends React.Component {
 					<a href="#">Logout</a>
 				</div>
 			</div>
-			
 			<button className="buttonCircular">
 				<img src={imageAbout} alt={"logo"} height="100%" width="100%"></img>
 			</button>
-
 		</div>
-		
     </div>
     );
   }
 }
-
 
 
 export default HelloMessage;
