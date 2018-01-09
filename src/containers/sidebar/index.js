a060bb56f7a9197ae7d020ca9f5ad51040d7b541
Seale Rapolai <1098005@students.wits.ac.za>
2018-01-09 14:34:33 +0200

    Chat preview reads from data store.

--- src/containers/sidebar/index.js
+++ src/containers/sidebar/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { push } from 'react-router-redux'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import Login from '../login';
 import Register from '../register';
 import Body from '../body';
@@ -14,31 +17,102 @@ import Avi from './avi.jpg';
 import Divider from 'material-ui/Divider';
 import './index.css';
 import MessagesContainer from "./messagesContainer"
+import {
+    new_Chat,
+    get_Chats
+} from '../../modules/chats'
 
-export default class Sidebar extends React.Component
-{
+class ChatBar extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            chats: [],
+        }
+    }
+    componentWillMount(props){
+        console.log("I am mounting")
+        //console.log(this.props.chatList);
+        var chatItems = this.props.chatList;
+        var msgs = [];
+        for(var item in chatItems){
+            console.log(item);
+            msgs.push(<MessagesContainer username={chatItems[item].name} message={chatItems[item].msgPreve}/>)
+        }
+        this.setState({
+            chats: msgs
+        })
+    }
     render()
-    {           
-        const sideBarMessage = [
-            <MessagesContainer username="Bunny 101 Memmber" message ="The practice of cigar smoking has been on the rise in the U.S. since the early 90’s"/>,
-            <MessagesContainer username="Bunny 101 Memmber" message ="The practice of cigar smoking has been on the rise in the U.S. since the early 90’s"/>,
-            <MessagesContainer username="Bunny 101 Memmber" message ="The practice of cigar smoking has been on the rise in the U.S. since the early 90’s"/>,
-        ]
+    {
         return (
-            
             <MuiThemeProvider>
-            <div>
-            <List>
-            <ListItem disabled={true}>
-                <SearchBar hintText="Search Chats" />
-            </ListItem>
-            <Divider />
-            
-           {sideBarMessage}
-            <Divider />
-        </List>
-            </div>
-        </MuiThemeProvider>
+                <List>
+                    <ListItem disabled={true}>
+                        <SearchBar hintText="Search Chats" />
+                    </ListItem>
+                    <Divider />
+                        {/*sideBarMessage*/}
+                        {this.state.chats}
+                    <Divider />
+                </List>
+            </MuiThemeProvider>
         );
     }
 }
+
+class NewChat extends React.Component{
+    render(){
+        return(
+            <MuiThemeProvider>
+                <List>
+                    <ListItem disabled={true}>
+                        <SearchBar hintText="Friends Email" />
+                    </ListItem>
+
+                </List>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+class Sidebar extends React.Component{
+    constructor(props){
+        super(props)
+        //console.log(props.chatList)
+        this.state = {
+            newChat: false,
+            userChats: props.chatList
+        }
+    }
+    componentWillReceiveProps(nextProps){
+        //console.log(nextProps.chats);
+        this.setState({
+            newChat: nextProps.NewChat
+        })
+    }
+    render(){
+        return(
+            <div>
+                {!this.state.newChat && <ChatBar chatList={this.state.userChats}/>}
+                {this.state.newChat && <NewChat />}
+            </div>
+        )
+    }
+}
+
+//export default Header;
+const mapStateToProps = (state) => ({
+    numChats: state.chats.numChats,
+    NewChat: state.chats.NewChat,
+    chatList: state.chats.chatList
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+	new_Chat,
+    get_Chats
+}, dispatch)
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Sidebar)
