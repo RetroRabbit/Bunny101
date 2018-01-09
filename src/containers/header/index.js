33fcfbedfd4eb69d64681458245d9b76df3c6a6b
retrotshepo <tmabine@retrorabbit.co.za>
2018-01-09 12:26:32 +0200

    header: changed component 'const' to 'class' added material-ui stuff

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -5,27 +5,50 @@ import Register from '../register'
 import Body from '../body'
 import imageProfile from './ic_account_circle_black_48dp.png'
 import imageAbout from './ic_schedule_black_48dp.png'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+
 import './index.css'
 
-const Header = () => (
-    <div id="header">
-		<button class="buttonRectangular">NEW CHAT</button>
+class HelloMessage extends React.Component {
+
+	
+  render() {
+    return (
+          <div id="header">
+		<button className="buttonRectangular">NEW CHAT</button>
 		
-		<button class="buttonRectangular">NEW GROUP</button>
+		<button className="buttonRectangular">NEW GROUP</button>
 		
 		<div id="accountSettings">
 			<p id="chatName">Eddie Logan</p>
 			
-			<button class="buttonCircular">
-				<img src={imageProfile} height="100%" width="100%"></img>
-			</button>
 			
-			<button class="buttonCircular">
+			<div className="dropDown">
+				<button className="buttonCircular" id="preferences">
+					<img src={imageProfile} height="100%" width="100%"></img>
+				</button>
+				<div className="dropDownContent">
+					<a href="#">Settings</a>
+					<a href="#">Logout</a>
+				</div>
+			</div>
+			
+			<button className="buttonCircular">
 				<img src={imageAbout} alt={"logo"} height="100%" width="100%"></img>
 			</button>
+
 		</div>
 		
     </div>
-)
+    );
+  }
+}
 
-export default Header;
+
+
+export default HelloMessage;
