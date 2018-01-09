8bbe58d5cd1a18a08ff81fa1aa8b1309aa3d2821
Nardus <nvandervyver@retrorabbit.co.za>
2018-01-09 13:46:12 +0200

    https://github.com/RetroRabbit/Bunny101/pull/28

--- src/containers/chat/account_screen/index.js
+++ src/containers/chat/account_screen/index.js
@@ -3,6 +3,21 @@ import './index.css'
 
 const Account_Screen = () => (
 <div class="chatbox">
+    <div className="user-msg">
+        <div class="sent-message-box">
+            <p class="message-text">Without a doubt there is.
+            </p>
+            <p class="sent-time">07H00</p>
+        </div>
+    </div>
+
+    <div className="user-msg">
+        <div class="received-message-box">
+            <p class="message-text">Without a doubt there is something very relaxing 
+            </p>
+            <p class="received-time">07H00</p>
+         </div>  
+    </div>
 
     <div className="user-msg">
         <div class="sent-message-box">
@@ -25,9 +40,10 @@ const Account_Screen = () => (
 
     <div className="user-msg">
         <div class="sent-message-box">
-            <p class="message-text">Without a doubt there is something very 
-            relaxing and pleasurable about cooking and eating grilled food.
-            To reduce the risks follow these basic tips:
+            <p class="message-text">Without a doubt there is something very relaxing and pleasurable about cooking
+            and eating grilled food. To reduce the risks follow these basic tips: 
+            Without a doubt there is something very relaxing and pleasurable about cooking 
+            and eating grilled food. To reduce the risks follow these basic tips:
             </p>
             <p class="sent-time">07H00</p>
         </div>     
