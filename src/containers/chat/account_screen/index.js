4e69287e144c91643041e19215eb8aa6a6289ff5
Nardus <nvandervyver@retrorabbit.co.za>
2018-01-09 13:34:11 +0200

    Fixed chat bubble alignment

--- src/containers/chat/account_screen/index.js
+++ src/containers/chat/account_screen/index.js
@@ -3,6 +3,15 @@ import './index.css'
 
 const Account_Screen = () => (
 <div class="chatbox">
+
+    <div className="user-msg">
+        <div class="sent-message-box">
+            <p class="message-text">Without a doubt there is.
+            </p>
+            <p class="sent-time">07H00</p>
+        </div>
+    </div>
+
     <div className="user-msg">
         <div class="received-message-box">
             <p class="message-text">Without a doubt there is something very relaxing and pleasurable about cooking
@@ -11,22 +20,17 @@ const Account_Screen = () => (
             and eating grilled food. To reduce the risks follow these basic tips:
             </p>
             <p class="received-time">07H00</p>
-         </div>
-        
-        
+         </div>  
     </div>
+
     <div className="user-msg">
         <div class="sent-message-box">
             <p class="message-text">Without a doubt there is something very 
             relaxing and pleasurable about cooking and eating grilled food.
             To reduce the risks follow these basic tips:
-            Without a doubt there is something very 
-            relaxing and pleasurable about cooking and eating grilled food.
-            To reduce the risks follow these basic tips:
             </p>
             <p class="sent-time">07H00</p>
-        </div>
-        
+        </div>     
     </div>    
 </div>       
 )
