d57857a5a115cfc924770a082a843d192e4e7c0a
Nardus <nvandervyver@retrorabbit.co.za>
2018-01-11 09:57:34 +0200

    Fixed errors

--- src/containers/settings/index.js
+++ src/containers/settings/index.js
@@ -12,56 +12,56 @@ import "./index.css"
 
 class Settings extends React.Component {
 
-constructor(props){
-    super(props)
-    this.state={
-        active: false, image: this.props.profilePic
+    constructor(props){
+        super(props)
+        this.state={
+            active: false, image: this.props.profilePic
+        }
+        console.log( this.props);
     }
-    console.log( this.props);
-}
-
-render() {
-    let $ProfileImage = null;
-
-if(this.state.image == '0' || this.props.profilePic == undefined)
-{
-  $ProfileImage = (<div class="profile-pic"><img src={Profile_Pic} ></img></div>);
-} else {
-  $ProfileImage = (<div class="profile-pic-selected"><img src={this.props.profilePic} class="profPic" ></img></div>);
-}
+    
 	render() {
+        let $ProfileImage = null;
+
+        if(this.state.image == '0' || this.props.profilePic == undefined)
+        {
+        $ProfileImage = (<div class="profile-pic"><img src={Profile_Pic} ></img></div>);
+        } else {
+        $ProfileImage = (<div class="profile-pic-selected"><img src={this.props.profilePic} class="profPic" ></img></div>);
+        }
+
 		return (
 			    <div className="settings">
-        <div className="header headSpace">
-            <Header />
-        </div>
-        <div className="settings-wrapper">
-            <div className="settings-content">
-                    <div class="profile-pic-container">
-                        {$ProfileImage}                               
+                    <div className="header headSpace">
+                        <Header />
                     </div>
-                    <div className="user-details">
-                        <div className="user-name">
-                            <h1 id="accountName">James Jones</h1>
+                    <div className="settings-wrapper">
+                        <div className="settings-content">
+                                <div class="profile-pic-container">
+                                    {$ProfileImage}                               
+                                </div>
+                                <div className="user-details">
+                                    <div className="user-name">
+                                        <h1 id="accountName">James Jones</h1>
+                                    </div>
+                                    <div className="user-email">
+                                        <h3 id="accountEmail">james.jones@gmail.com 
+                                            <h3 id="editPencil" onClick={
+                                                () => this.props.openForm()}>
+                                                <FontAwesome name='pencil'/>
+                                            </h3>
+                                        </h3>
+                                    </div>		
+                                    
+                                </div>
+                                <div className="controls" onClick={
+                                    () => this.props.changeToBody()
+                                }
+                                ><button id="buttonDone">DONE</button>
+                                </div>
                         </div>
-                        <div className="user-email">
-                            <h3 id="accountEmail">james.jones@gmail.com 
-								<h3 id="editPencil" onClick={
-									() => this.props.openForm()}>
-									<FontAwesome name='pencil'/>
-								</h3>
-							</h3>
-                        </div>		
-						
                     </div>
-                    <div className="controls" onClick={
-						() => this.props.changeToBody()
-					}
-					><button id="buttonDone">DONE</button>
-                    </div>
-            </div>
-        </div>
-    </div>
+                </div>
 		);
   }
 }
