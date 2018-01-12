5d3c3e6939627b4a5a89589ecebac3ea647da4c6
Thabang Lekwadi <tlekwadi@retrorabbit.co.za>
2018-01-12 07:50:09 +0200

    Correct API

---
+++ Bunny101APICore/Bunny101APICore/Controllers/ChatsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Bunny101APICore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bunny101APICore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Chats")]
+    public class ChatsController : Controller
+    {
+        private ApplicationDBContext db;
+
+        /* Add method to change read message */
+        public ChatsController(ApplicationDBContext context)
+        {
+            //By dependency injection rules we get this context in the startup file 
+            db = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<ChatMessage> Get()
+        {
+            //Get All Conversations 
+            var chats = db.ChatMessages.ToList();
+
+            return chats;
+        }
+
+        [HttpGet("{id:int}")]
+        public ChatMessage Get(int id)
+        {
+            var chatMessage = db.ChatMessages.Find(id);
+
+            return chatMessage;
+        }
+
+        [HttpPost]
+        public int Add([FromBody]ChatMessage message)
+        {
+            db.ChatMessages.Add(message);
+
+            db.SaveChanges();
+
+            return message.Id;
+        }
+
+        [HttpGet("{id}")]
+        [Route("GetConversation")]
+        public List<ChatMessage> Get(string r, string s)
+        {
+            //get conversation between two users 
+            List<ChatMessage> conversation = new List<ChatMessage>();
+
+            conversation = db.ChatMessages.Where(e => (e.RecieverEmail == r && e.SenderEmail == s) || (e.RecieverEmail == s && e.SenderEmail == r)).ToList();
+
+            return conversation;
+        }
+
+        //[HttpGet("{id}")]
+        //[Route("GetRecentMessage")]
+        //public List<ChatMessage> Get(string id)
+        //{
+        //    //get conversation between two users 
+        //    List<ChatMessage> conversation = new List<ChatMessage>();
+
+        //    conversation = db.ChatMessages.Where(e => (e.RecieverEmail == r && e.SenderEmail == s) || (e.RecieverEmail == s && e.SenderEmail == r)).ToList();
+
+        //    return conversation;
+        //}
+
+    }
+}
+ 
---
+++ Bunny101APICore/Bunny101APICore/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Bunny101APICore.Controllers;
+
+namespace Bunny101APICore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddCors();
+
+            services.AddSingleton(Configuration);
+            services.AddScoped<ChatsController>();
+
+            var connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MessagesDB;Integrated Security=True;Connect Timeout=30;";
+            services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(connection));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
